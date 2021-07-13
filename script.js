ba9ba25a09df25ae2854d9efbdb479d989559a32
Shalinipal301 <55969368+Shalinipal301@users.noreply.github.com>
2021-07-13 23:49:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+const apikey = "3265874a2c77ae4a04bb96236a642d2f";
+
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
+const url = (city) =>
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
+
+async function getWeatherByLocation(city) {
+    const resp = await fetch(url(city), { origin: "cors" });
+    const respData = await resp.json();
+
+    console.log(respData);
+
+    addWeatherToPage(respData);
+}
+
+function addWeatherToPage(data) {
+    const temp = KtoC(data.main.temp);
+
+    const weather = document.createElement("div1");
+    weather.classList.add("weather");
+
+    weather.innerHTML = `<h2> <td scope="row" class="temp-title" style="font-size:40px"> &#x1F321; ${temp}°C</h2> `;
+
+    
+
+    // cleanup
+    main.innerHTML = "";
+
+    main.appendChild(weather);
+}
+
+function KtoC(K) {
+    return Math.floor(K - 273.15);
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const city = search.value;
+
+    if (city) {
+        getWeatherByLocation(city);
+    }
+});
